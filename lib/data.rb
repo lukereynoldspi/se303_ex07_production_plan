44f0865e43541e5f9a617dcb0e2ad8f14a175377
lukereynoldspi <lukereynoldspi@gmail.com>
2022-05-16 00:10:50 -0700

    added method to producer_test.rb

--- lib/data.rb
+++ lib/data.rb
@@ -12,3 +12,12 @@ def sample_province_data
     price: 20
   }
 end
+
+def sample_producer_data
+  {
+    name: "String producers",
+    producers: [""],
+    demand: 30,
+    price: 20
+  }
+end
--- test/producer_test.rb
+++ test/producer_test.rb
@@ -2,7 +2,14 @@ gem 'minitest', '~> 5.4'
 require 'minitest/autorun'
 require 'minitest/pride'
 require_relative '../lib/producer'
-
+require_relative '../lib/data'
 class ProducerTest < Minitest::Test
-   
+
+  def test_producer_shortfall
+    prov = Producer.new('string for producers', sample_producer_data)
+    assert_raises NoMethodError do
+      assert_equal(0, prov.shortfall) # Fowler said would not pass
+    end
+  end
+
 end
