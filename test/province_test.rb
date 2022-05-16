3eb956daf02a05f2d19602769d4a5d4673e1d44d
lukereynoldspi <lukereynoldspi@gmail.com>
2022-05-16 00:37:50 -0700

    added no producer shortfall test

--- test/province_test.rb
+++ test/province_test.rb
@@ -5,7 +5,6 @@ require_relative '../lib/province'
 require_relative '../lib/data'
 
 class ProvinceTest < Minitest::Test
-
   def setup
     @asia = Province.new(sample_province_data)
   end
@@ -22,5 +21,23 @@ class ProvinceTest < Minitest::Test
     @asia.producers[0].production = 20
     assert_equal(-6, @asia.shortfall)
     assert_equal(292, @asia.profit)
+  end
+
+  
+end
+
+class NoProducersTest < Minitest::Test
+  def setup
+    data = {
+      name: "No proudcers",
+      producers: [],
+      demand: 30,
+      price: 20
+    }
+    @no_producers = Province.new(data)
+  end
+
+  def test_no_producers_shortfall
+    assert_equal(30, @no_producers.shortfall)
   end
 end
