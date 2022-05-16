4b71e4a01dff1fb15f9c28d4c07ad55ce5e87796
lukereynoldspi <lukereynoldspi@gmail.com>
2022-05-16 00:39:52 -0700

    added test for no producers profit

--- test/province_test.rb
+++ test/province_test.rb
@@ -40,4 +40,8 @@ class NoProducersTest < Minitest::Test
   def test_no_producers_shortfall
     assert_equal(30, @no_producers.shortfall)
   end
+
+  def test_no_producers_shortfall
+    assert_equal(0, @no_producers.profit)
+  end
 end
